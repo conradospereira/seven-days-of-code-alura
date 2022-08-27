22d175441836622c515a56291aa2d62bdb55d9e1
conrado <conrado.spereira@gmail.com>
2022-08-26 23:14:20 -0300

    Create Controller.

--- src/main/java/com/sevendaysofcode/imdbapi/ImdbapiApplication.java
+++ src/main/java/com/sevendaysofcode/imdbapi/ImdbapiApplication.java
@@ -1,13 +1,17 @@
 package com.sevendaysofcode.imdbapi;
 
+import com.sevendaysofcode.imdbapi.Controller.ApiController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImdbapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImdbapiApplication.class, args);
-	}
+	public static void main(String[] args) throws Exception{
 
+		SpringApplication.run(ImdbapiApplication.class, args);
+		ApiController apiController = new ApiController();
+
+		apiController.getTopMovies();
+	}
 }
